d83e844d721b6a62005c8bea5436be76530b5f4e
bayurama30 <bayurama30@gmail.com>
2021-02-25 12:21:00 +0700

    menambahkan template dashboard pada mockup pijar

---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,29 @@
+import React, { lazy } from 'react'
+import {
+  CBadge,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CCallout
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import Tabel from './Tabel'
+
+import MainChartExample from '../charts/MainChartExample.js'
+
+const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
+const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
+
+const Dashboard = () => {
+  return(
+    <Tabel/>
+  )
+}
+
+export default Dashboard
